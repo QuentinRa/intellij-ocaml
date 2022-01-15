acc9f095e355159e95fdf9dc9da75f242cf881d6
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2022-01-15 16:43:31 +0100

    testing format done

--- test/main/com/ocaml/ide/sdk/OCamlSdkUtilsTest.java
+++ test/main/com/ocaml/ide/sdk/OCamlSdkUtilsTest.java
@@ -1,6 +1,7 @@
 package com.ocaml.ide.sdk;
 
 import com.intellij.openapi.options.*;
+import com.intellij.openapi.projectRoots.*;
 import com.ocaml.*;
 import org.junit.*;
 
@@ -20,6 +21,37 @@ import org.junit.*;
 @SuppressWarnings("JUnit4AnnotatedMethodInJUnit3TestCase")
 public class OCamlSdkUtilsTest extends OCamlBaseTest {
 
+    // assuming that ocaml IS installed on this path
+    protected static final String V_OCAML_BINARY_WSL = "\\\\wsl$\\Debian\\bin\\ocaml";
+    protected static final String V_OCAML_COMPILER_WSL = "\\\\wsl$\\Debian\\bin\\ocamlc";
+    protected static final String V_OCAML_VERSION_WSL = "4.12.0";
+    protected static final String V_OCAML_SOURCES_FOLDER_WSL = "\\\\wsl$\\Debian\\usr\\lib\\ocaml";
+
+    // assuming that ocaml IS NOT installed on this path
+    protected static final String I_OCAML_BINARY_WSL = "\\\\wsl$\\Debian\\home\\bin\\ocaml";
+    protected static final String I_OCAML_COMPILER_WSL = "\\\\wsl$\\Debian\\home\\bin\\ocamlc";
+    protected static final String I_OCAML_VERSION_WSL = "4.12.0";
+    protected static final String I_OCAML_SOURCES_FOLDER_WSL = "\\\\wsl$\\Debian\\home\\usr\\lib\\ocaml";
+
+    // assuming that this path is "valid", but the WSL distribution is invalid
+    protected static final String N_NAME_WSL = "Fedora";
+    protected static final String N_OCAML_BINARY_WSL = "\\\\wsl$\\Fedora\\bin\\ocaml";
+    protected static final String N_OCAML_COMPILER_WSL = "\\\\wsl$\\Fedora\\bin\\ocamlc";
+    protected static final String N_OCAML_VERSION_WSL = "4.12.0";
+    protected static final String N_OCAML_SOURCES_FOLDER_WSL = "\\\\wsl$\\Fedora\\usr\\lib\\ocaml";
+
+    // assuming that ocaml IS installed on this path
+    protected static final String V_OCAML_BINARY_WIN = "C:\\cygwin64\\bin\\ocaml";
+    protected static final String V_OCAML_COMPILER_WIN = "C:\\cygwin64\\bin\\ocamlc";
+    protected static final String V_OCAML_VERSION_WIN = "4.10.0";
+    protected static final String V_OCAML_SOURCES_FOLDER_WIN = "C:\\cygwin64\\lib\\ocaml";
+
+    // assuming that ocaml IS NOT installed on this path
+    protected static final String I_OCAML_BINARY_WIN = "C:\\cygwin\\bin\\ocaml";
+    protected static final String I_OCAML_COMPILER_WIN = "C:\\cygwin\\bin\\ocamlc";
+    protected static final String I_OCAML_VERSION_WIN = "4.10.0";
+    protected static final String I_OCAML_SOURCES_FOLDER_WIN = "C:\\cygwin\\lib\\ocaml";
+
     private static final class CreateSdkParams {
         public String ocamlBinary;
         public String ocamlCompiler;
@@ -36,8 +68,17 @@ public class OCamlSdkUtilsTest extends OCamlBaseTest {
         private static CreateSdkParams validWSL() {
             return new CreateSdkParams(V_OCAML_BINARY_WSL, V_OCAML_COMPILER_WSL, V_OCAML_VERSION_WSL, V_OCAML_SOURCES_FOLDER_WSL);
         }
+        private static CreateSdkParams nonExistingWSL() {
+            return new CreateSdkParams(N_OCAML_BINARY_WSL, N_OCAML_COMPILER_WSL, N_OCAML_VERSION_WSL, N_OCAML_SOURCES_FOLDER_WSL);
+        }
         private static CreateSdkParams invalidWSL() {
             return new CreateSdkParams(I_OCAML_BINARY_WSL, I_OCAML_COMPILER_WSL, I_OCAML_VERSION_WSL, I_OCAML_SOURCES_FOLDER_WSL);
+        }
+        private static CreateSdkParams validWin() {
+            return new CreateSdkParams(V_OCAML_BINARY_WIN, V_OCAML_COMPILER_WIN, V_OCAML_VERSION_WIN, V_OCAML_SOURCES_FOLDER_WIN);
+        }
+        private static CreateSdkParams invalidWin() {
+            return new CreateSdkParams(I_OCAML_BINARY_WIN, I_OCAML_COMPILER_WIN, I_OCAML_VERSION_WIN, I_OCAML_SOURCES_FOLDER_WIN);
         }
     }
 
@@ -48,6 +89,20 @@ public class OCamlSdkUtilsTest extends OCamlBaseTest {
                 params.ocamlCompiler,
                 params.ocamlSources
         ));
+    }
+    public void assertConfigurationOK(CreateSdkParams params) {
+        try {
+            Sdk sdk = OCamlSdkUtils.createSdk(
+                    params.ocamlBinary,
+                    params.version,
+                    params.ocamlCompiler,
+                    params.ocamlSources
+            );
+            // todo: check SDK != null
+            assertTrue(true);
+        } catch (ConfigurationException e) {
+            fail(e.getMessage());
+        }
     }
 
     //
@@ -75,24 +130,66 @@ public class OCamlSdkUtilsTest extends OCamlBaseTest {
     //
     // An invalid binary does an ends with "/bin/ocaml" (resp. "/bin/ocaml", "/lib/ocaml")
     //
-
+    // WSL: 1/5, the binary is invalid
     @Test
-    public void testBinaryInvalidFormat() {
+    public void testBinaryInvalidFormatWSL() {
         CreateSdkParams params = CreateSdkParams.validWSL();
         params.ocamlBinary = "/bin/";
         assertConfigurationFailed(OCamlBundle.message("sdk.ocaml.binary.invalid"), params);
     }
-
+    // WSL: 2/5, the sources' folder is invalid
     @Test
-    public void testSourceInvalidFormat() {
+    public void testSourceInvalidFormatWSL() {
         CreateSdkParams params = CreateSdkParams.validWSL();
-        params.ocamlSources = "/bin/";
+        params.ocamlSources = "/lib/";
         assertConfigurationFailed(OCamlBundle.message("sdk.ocaml.sources.folder.invalid"), params);
     }
-
-    @Test // the WSL do not exist
+    // WSL: 3/5, every value is valid
+    @Test
+    public void testSourceValidFormatWSL() {
+        CreateSdkParams params = CreateSdkParams.validWSL();
+        assertConfigurationOK(params);
+    }
+    // WSL: 4/5, every value is valid, but the WSL do not exist
+    @Test
     public void testSourceInvalidWSL() {
-        CreateSdkParams params = CreateSdkParams.invalidWSL();
-        assertConfigurationFailed(OCamlBundle.message("sdk.path.binary.wsl.invalid", I_NAME_WSL), params);
+        CreateSdkParams params = CreateSdkParams.nonExistingWSL();
+        assertConfigurationFailed(
+                OCamlBundle.message("sdk.path.binary.wsl.invalid", N_NAME_WSL), params
+        );
+    }
+    // WSL: 5/5, valid, but sources' folder ends with a trailing slash
+    @Test
+    public void testSourceExtraFolderSeparatorWSL() {
+        CreateSdkParams params = CreateSdkParams.validWSL();
+        params.ocamlSources += "/";
+        assertConfigurationOK(params);
+    }
+    // Windows: 1/3, every value is valid
+    @Test
+    public void testBinaryInvalidFormatWin() {
+        CreateSdkParams params = CreateSdkParams.validWin();
+        params.ocamlBinary = "\\bin\\";
+        assertConfigurationFailed(OCamlBundle.message("sdk.ocaml.binary.invalid"), params);
+    }
+    // Windows: 2/3, every value is valid
+    @Test
+    public void testSourceInvalidFormatWin() {
+        CreateSdkParams params = CreateSdkParams.validWin();
+        params.ocamlSources = "\\lib";
+        assertConfigurationFailed(OCamlBundle.message("sdk.ocaml.sources.folder.invalid"), params);
+    }
+    // Windows: 3/4, every value is valid
+    @Test
+    public void testSourceValidFormatWin() {
+        CreateSdkParams params = CreateSdkParams.validWin();
+        assertConfigurationOK(params);
+    }
+    // Windows: 4/4, valid, but sources' folder ends with a trailing slash
+    @Test
+    public void testSourceExtraFolderSeparatorWin() {
+        CreateSdkParams params = CreateSdkParams.validWin();
+        params.ocamlSources += "\\";
+        assertConfigurationOK(params);
     }
 }
