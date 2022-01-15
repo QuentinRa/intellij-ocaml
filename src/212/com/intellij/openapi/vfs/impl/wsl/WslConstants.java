be520b757baed2b24bc12d9a7bdee193f4999c63
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2022-01-15 15:59:47 +0100

    support for 211 212

---
+++ src/212/com/intellij/openapi/vfs/impl/wsl/WslConstants.java
@@ -0,0 +1,7 @@
+package com.intellij.openapi.vfs.impl.wsl;
+
+import com.intellij.execution.wsl.*;
+
+public final class WslConstants {
+    public static final String UNC_PREFIX = WSLDistribution.UNC_PREFIX;
+}
