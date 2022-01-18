a6f8b95dca4bd323dba0fa0e4440f3cca9f51773
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2022-01-18 14:50:58 +0100

    working on #7 - remove the Makefile from the code

--- src/main/com/ocaml/ide/projectWizard/templates/OCamlMakefileTemplate.java
+++ src/main/com/ocaml/ide/projectWizard/templates/OCamlMakefileTemplate.java
@@ -51,50 +51,9 @@ class OCamlMakefileTemplate implements ProjectTemplate, TemplateBuildInstruction
         OCamlFileUtils.createFile(sourceRootFile, "hello_world.mli", "val hello_world : unit -> unit");
         OCamlFileUtils.createFile(sourceRootFile, "hello_world.ml", "let hello_world () = Format.printf \"Hello, World!@.\"");
         OCamlFileUtils.createFile(sourceRootFile, "test_hello_world.mli","open Hello_world\n\nlet _ = hello_world ()");
-        File Makefile = OCamlFileUtils.createFile(
-                sourceRootFile.getParentFile(),
-                "Makefile",
-                "# DO NOT REMOVE THOSE LINE\n" +
-                        "OUTPUT=./out/\n" +
-                        "SOURCE=./src/\n" +
-                        "# DO NOT REMOVE THOSE LINE\n" +
-                        "\n" +
-                        "# we are calling init, then test_hello_world\n" +
-                        "all: init $(OUTPUT)test_hello_world\n" +
-                        "\n" +
-                        "#######################################\n" +
-                        "# generic rules to compile our files  #\n" +
-                        "#######################################\n" +
-                        "$(OUTPUT)%.cmi: $(SOURCE)%.mli\n" +
-                        "\tocamlc -c $< -o $@ -I $(OUTPUT) -bin-annot\n" +
-                        "\n" +
-                        "$(OUTPUT)%.cmo: $(SOURCE)%.ml\n" +
-                        "\tocamlc -c $< -o $@ -I $(OUTPUT) -bin-annot\n" +
-                        "\n" +
-                        "##############################\n" +
-                        "# Rules to cmpile our files  #\n" +
-                        "##############################\n" +
-                        "\n" +
-                        "# we need hello_world.cmo and test_hello_world.cmo to create test_hello_world\n" +
-                        "$(OUTPUT)test_hello_world: $(OUTPUT)hello_world.cmo $(OUTPUT)test_hello_world.cmo\n" +
-                        "\tocamlc $^ -o $@ -I $(OUTPUT) -bin-annot\n" +
-                        "# we need to compile the interface first to get hello_world.cmo\n" +
-                        "$(OUTPUT)hello_world.cmo: $(OUTPUT)hello_world.cmi\n" +
-                        "# we need to compile hello_world.cmo first\n" +
-                        "$(OUTPUT)test_hello_world.cmo: $(OUTPUT)hello_world.cmo\n" +
-                        "\n" +
-                        "##########\n" +
-                        "# Tasks  #\n" +
-                        "##########\n" +
-                        "\n" +
-                        "init:\n" +
-                        "\tmkdir -p $(OUTPUT)\n" +
-                        "\n" +
-                        "clean:\n" +
-                        "\trm -rf $(OUTPUT)\n" +
-                        "\n" +
-                        ".PHONY: clean"
-        );
+
+        String makefileContent = OCamlFileUtils.loadFileContent("/templates/Makefile/Makefile");
+        File Makefile = OCamlFileUtils.createFile(sourceRootFile.getParentFile(), "Makefile", makefileContent);
 
         if (Makefile != null) {
             PsiUtils.openFile(rootModel.getProject(), Makefile, true);
--- src/main/com/ocaml/utils/files/OCamlFileUtils.java
+++ src/main/com/ocaml/utils/files/OCamlFileUtils.java
@@ -1,5 +1,6 @@
 package com.ocaml.utils.files;
 
+import com.intellij.openapi.vfs.*;
 import org.jetbrains.annotations.*;
 
 import java.io.*;
@@ -30,9 +31,26 @@ public final class OCamlFileUtils {
     }
 
     /**
-     * Create a file, with some text.
-     * todo: deprecate, copy of file from a template would be better I think
+     * Load a file stored in resources.
+     * The path must starts with a /.
+     * The separators for the lines in the returned file will be \n.
      */
+    public static @NotNull String loadFileContent(@NotNull String filePath) {
+        try {
+            var url = OCamlFileUtils.class.getClassLoader().getResource(filePath.replaceFirst("/", ""));
+            if (url == null) throw new IOException("Couldn't get URL for "+filePath);
+            VirtualFile virtualFile = VfsUtil.findFileByURL(url);
+            if (virtualFile == null) throw new IOException("Couldn't find file by URL for "+filePath);
+            String text = VfsUtil.loadText(virtualFile);
+            String[] split = text.split("\r\n");
+            return String.join("\n", split);
+        } catch (IOException e) {
+            // log: log this exception
+            return "";
+        }
+    }
+
+    /** Create a file with some text. */
     @Nullable public static File createFile(@NotNull File sourceRootFile,
                                             @NotNull String fileName, @NotNull String text) {
         try {
