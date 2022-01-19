02ff9112382f9bd112b66799c322d3a150e56936
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2022-01-19 22:28:55 +0100

    adding back version

---
+++ test/main/com/ocaml/sdk/version/VersionRegexPathTest.java
@@ -0,0 +1,83 @@
+package com.ocaml.sdk.version;
+
+import com.ocaml.OCamlBaseTest;
+import com.ocaml.compiler.OCamlSdkVersionManager;
+import org.junit.Test;
+
+@SuppressWarnings("JUnit4AnnotatedMethodInJUnit3TestCase")
+public final class VersionRegexPathTest extends OCamlBaseTest {
+
+    private void assertIsUnknownVersion(String path) {
+        assertTrue(OCamlSdkVersionManager.isUnknownVersion(OCamlSdkVersionManager.parse(path)));
+    }
+
+    private void assertIsVersion(String path, String expectedVersion) {
+        assertEquals(expectedVersion, OCamlSdkVersionManager.parse(path));
+    }
+
+    @Test
+    public void testEmpty() {
+        assertIsUnknownVersion("");
+    }
+
+    @Test
+    public void testNotAPath() {
+        assertIsUnknownVersion("4.05.0");
+    }
+
+    @Test
+    public void test2d() {
+        assertIsUnknownVersion("/40.5");
+        assertIsUnknownVersion("/40.50");
+    }
+
+    @Test
+    public void test1dThen2d() {
+        assertIsVersion("~/.opam/4.05", "4.05");
+        assertIsVersion("~/.opam/4.05/", "4.05");
+        assertIsVersion("~\\.opam\\4.05", "4.05");
+        assertIsVersion("~\\.opam\\4.05/", "4.05");
+    }
+
+    @Test
+    public void test1dThen2dThenEmpty() {
+        assertIsUnknownVersion("~/.opam/4.05.");
+        assertIsUnknownVersion("~\\.opam\\4.05.");
+    }
+
+    @Test
+    public void test1dThen2dThen1d() {
+        assertIsVersion("~/.opam/4.05.2", "4.05.2");
+        assertIsVersion("~/.opam/4.05.2/", "4.05.2");
+        assertIsVersion("~\\.opam\\4.05.2", "4.05.2");
+        assertIsVersion("~\\.opam\\4.05.2\\", "4.05.2");
+    }
+
+    @Test
+    public void test1dThen2dThen2d() {
+        assertIsUnknownVersion("~/.opam/4.05.02");
+        assertIsUnknownVersion("~/.opam/4.05.02/");
+        assertIsUnknownVersion("~\\.opam\\4.05.02");
+        assertIsUnknownVersion("~\\.opam\\4.05.02\\");
+    }
+
+    @Test
+    public void test1dThen2dThen2dThenMingw() {
+        assertIsVersion("~/.opam/4.13.1+mingw64c", "4.13.1+mingw64c");
+        assertIsVersion("~/.opam/4.13.1+mingw64c/", "4.13.1+mingw64c");
+        assertIsVersion("~\\.opam\\4.13.1+mingw64c", "4.13.1+mingw64c");
+        assertIsVersion("~\\.opam\\4.13.1+mingw64c\\", "4.13.1+mingw64c");
+    }
+
+    @Test
+    public void test1dThen2dThen2dThenFake() {
+        assertIsVersion("~/.opam/4.12.0-v", "4.12.0");
+        assertIsVersion("~/.opam/4.12.0-v2", "4.12.0");
+        assertIsVersion("~/.opam/4.12.0-v/", "4.12.0");
+        assertIsVersion("~/.opam/4.12.0-v2/", "4.12.0");
+        assertIsVersion("~\\.opam\\4.12.0-v", "4.12.0");
+        assertIsVersion("~\\.opam\\4.12.0-v2", "4.12.0");
+        assertIsVersion("~\\.opam\\4.12.0-v\\", "4.12.0");
+        assertIsVersion("~\\.opam\\4.12.0-v2\\", "4.12.0");
+    }
+}
---
+++ test/main/com/ocaml/sdk/version/VersionRegexTest.java
@@ -0,0 +1,71 @@
+package com.ocaml.sdk.version;
+
+import com.ocaml.OCamlBaseTest;
+import com.ocaml.compiler.OCamlSdkVersionManager;
+import org.junit.Test;
+
+@SuppressWarnings("JUnit4AnnotatedMethodInJUnit3TestCase")
+public final class VersionRegexTest extends OCamlBaseTest {
+
+    // invalid
+
+    @Test
+    public void test1d() {
+        assertFalse(OCamlSdkVersionManager.isValid("4"));
+    }
+
+    @Test
+    public void test1dEmpty() {
+        assertFalse(OCamlSdkVersionManager.isValid("4."));
+    }
+
+    @Test
+    public void test1d1d() {
+        assertFalse(OCamlSdkVersionManager.isValid("4.0"));
+    }
+
+    @Test
+    public void test1d1dEmpty() {
+        assertFalse(OCamlSdkVersionManager.isValid("4.0."));
+    }
+
+    @Test
+    public void test1d1d1d() {
+        assertFalse(OCamlSdkVersionManager.isValid("4.0.0"));
+    }
+
+    @Test
+    public void testInvalidStart() {
+        assertFalse(OCamlSdkVersionManager.isValid("ocaml-4.05.0"));
+    }
+
+    // valid
+
+    @Test
+    public void test1d2d() {
+        assertTrue(OCamlSdkVersionManager.isValid("4.05"));
+    }
+
+    @Test
+    public void test1d2dKind() {
+        assertTrue(OCamlSdkVersionManager.isValid("4.05+mingw64c"));
+    }
+
+    @Test
+    public void test1d2d1d() {
+        assertTrue(OCamlSdkVersionManager.isValid("4.05.0"));
+    }
+
+    @Test
+    public void test1d2d1dKind() {
+        assertTrue(OCamlSdkVersionManager.isValid("4.05.0+mingw64c"));
+    }
+
+    @Test
+    public void test1d2d1dSpecial() {
+        assertTrue(OCamlSdkVersionManager.isValid("4.05.0-v"));
+        assertTrue(OCamlSdkVersionManager.isValid("4.05.0-v2"));
+        // invalid inside "valid", because they are altogether special
+        assertFalse(OCamlSdkVersionManager.isValid("4.05.0-"));
+    }
+}
