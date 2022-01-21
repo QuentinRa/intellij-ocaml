d25195fc154e5e133c68d9d8bbcdaa4db945adf1
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2022-01-21 18:19:04 +0100

    #27 done, hide files that are not ending with .ml, .mli, or .cmt

--- src/main/com/ocaml/sdk/nodes/OCamlLibraryRootsNodeDecorator.java
+++ src/main/com/ocaml/sdk/nodes/OCamlLibraryRootsNodeDecorator.java
@@ -1,18 +1,14 @@
-package com.ocaml.sdk;
+package com.ocaml.sdk.nodes;
 
 import com.intellij.ide.projectView.PresentationData;
 import com.intellij.ide.projectView.ProjectViewNode;
 import com.intellij.ide.projectView.ProjectViewNodeDecorator;
-import com.intellij.ide.projectView.impl.nodes.NamedLibraryElement;
-import com.intellij.ide.projectView.impl.nodes.NamedLibraryElementNode;
 import com.intellij.ide.projectView.impl.nodes.PsiDirectoryNode;
 import com.intellij.ide.util.treeView.NodeDescriptor;
-import com.intellij.openapi.projectRoots.Sdk;
-import com.intellij.openapi.roots.JdkOrderEntry;
-import com.intellij.openapi.roots.OrderEntry;
 import com.intellij.packageDependencies.ui.PackageDependenciesNode;
 import com.intellij.ui.ColoredTreeCellRenderer;
 import com.ocaml.icons.OCamlIcons;
+import com.ocaml.sdk.utils.OCamlSdkRootsManager;
 
 /**
  * Add an icon to the nodes in "External libraries" that
@@ -25,16 +21,8 @@ public class OCamlLibraryRootsNodeDecorator implements ProjectViewNodeDecorator 
 
         // get library node
         NodeDescriptor<?> parentDescriptor = node.getParentDescriptor();
-        if (!(parentDescriptor instanceof NamedLibraryElementNode)) return;
-        NamedLibraryElement library = ((NamedLibraryElementNode) parentDescriptor).getValue();
-        if (library == null) return;
+        if (!OCamlSdkRootsManager.isLibraryRootForOCamlSdk(parentDescriptor)) return;
 
-        // check SDK
-        OrderEntry orderEntry = library.getOrderEntry();
-        if (!(orderEntry instanceof JdkOrderEntry)) return;
-        Sdk sdk = ((JdkOrderEntry) orderEntry).getJdk();
-        if (sdk == null) return;
-        if (!(sdk.getSdkType() instanceof OCamlSdkType)) return;
         // set icon
         data.setIcon(OCamlIcons.Nodes.OCAML_LIBRARY);
     }
