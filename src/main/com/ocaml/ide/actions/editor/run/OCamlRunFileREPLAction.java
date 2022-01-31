2e455596288a228582a316da7b34cec34848dbcb
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2022-01-31 21:22:23 +0100

    add notification if the REPL was not started

--- src/main/com/ocaml/ide/actions/editor/run/OCamlRunFileREPLAction.java
+++ src/main/com/ocaml/ide/actions/editor/run/OCamlRunFileREPLAction.java
@@ -1,13 +1,17 @@
 package com.ocaml.ide.actions.editor.run;
 
+import com.intellij.notification.NotificationType;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.ui.MessageType;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.ocaml.OCamlBundle;
 import com.ocaml.ide.actions.editor.OCamlEditorActionBase;
 import com.ocaml.ide.console.OCamlConsoleRunner;
 import com.ocaml.ide.console.OCamlConsoleToolWindowFactory;
+import com.ocaml.utils.notifications.OCamlNotificationData;
+import com.ocaml.utils.notifications.OCamlNotifications;
 import org.jetbrains.annotations.NotNull;
 
 public class OCamlRunFileREPLAction extends OCamlEditorActionBase {
@@ -23,8 +27,12 @@ public class OCamlRunFileREPLAction extends OCamlEditorActionBase {
 
     @Override public void actionPerformed(@NotNull AnActionEvent e) {
         OCamlConsoleRunner runner = OCamlConsoleToolWindowFactory.getOCamlConsoleRunner();
-        if (runner == null) { // todo: bundle
-            System.out.println("Cannot execute: you need to start the console first");
+        if (runner == null) {
+            // show notification so that the user know that he/she must add sources manually
+            var notification = new OCamlNotificationData(OCamlBundle.message("repl.no.started.desc"));
+            notification.mySubtitle = OCamlBundle.message("repl.no.started.title");
+            notification.myNotificationType = NotificationType.ERROR;
+            OCamlNotifications.notify(notification);
             return;
         }
         VirtualFile file = e.getData(CommonDataKeys.VIRTUAL_FILE);
