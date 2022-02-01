62e7a0ffa6821ee1210a66538f902d683c8532fc
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2022-02-01 18:51:51 +0100

    importing "odoc"

---
+++ src/main/com/odoc/utils/logs/OdocLogger.java
@@ -0,0 +1,19 @@
+package com.odoc.utils.logs;
+
+import com.intellij.openapi.diagnostic.Logger;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Create a logger for the plugin<br>
+ * See https://plugins.jetbrains.com/docs/intellij/ide-infrastructure.html#error-reporting
+ */
+public class OdocLogger {
+
+    public static @NotNull Logger getInstance() {
+        return Logger.getInstance("odoc");
+    }
+
+    public static @NotNull Logger getInstance(String s) {
+        return Logger.getInstance("odoc." + s);
+    }
+}
