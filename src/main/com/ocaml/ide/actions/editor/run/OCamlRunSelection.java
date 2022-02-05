2af4bba5be4ec80ffc1c925d035ca32d88fcbc6c
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2022-02-06 00:17:53 +0100

    fix no icon for RunSelection

--- src/main/com/ocaml/ide/actions/editor/run/OCamlRunSelection.java
+++ src/main/com/ocaml/ide/actions/editor/run/OCamlRunSelection.java
@@ -3,8 +3,10 @@ package com.ocaml.ide.actions.editor.run;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.editor.Editor;
+import com.ocaml.icons.OCamlIcons;
 import com.ocaml.ide.actions.editor.OCamlEditorActionBase;
 import com.ocaml.ide.console.OCamlConsoleRunner;
+import com.ocaml.utils.adaptor.SinceIdeVersion;
 import com.ocaml.utils.editor.ExtendedEditorActionUtil;
 import org.jetbrains.annotations.NotNull;
 
@@ -15,6 +17,12 @@ import org.jetbrains.annotations.NotNull;
 public class OCamlRunSelection extends OCamlEditorActionBase {
 
     public static final String ACTION_ID = "editor.repl.run.selection.action";
+
+    @SinceIdeVersion(release = "211")
+    public OCamlRunSelection() {
+        // fix 211, can't use the package icon if not in main/icons
+        getTemplatePresentation().setIcon(OCamlIcons.External.ExecuteSelection);
+    }
 
     @Override protected void doActionPerformed(@NotNull AnActionEvent e, OCamlConsoleRunner runner) {
         Editor editor = e.getData(CommonDataKeys.EDITOR);
