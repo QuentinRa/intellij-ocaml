a1dcdafc86893a9858afe67e2632ca7efb552ddf
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2022-02-07 18:32:06 +0100

    fix problem with the plugin verifier

--- src/main/com/ocaml/ide/console/debug/groups/TreeElementGroupKind.java
+++ src/main/com/ocaml/ide/console/debug/groups/TreeElementGroupKind.java
@@ -2,7 +2,6 @@ package com.ocaml.ide.console.debug.groups;
 
 import com.ocaml.OCamlBundle;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.PropertyKey;
 
 // it's not safe to add a value here
 // ensure that you know were they are used before adding one
@@ -12,15 +11,15 @@ import org.jetbrains.annotations.PropertyKey;
 // - ShowElementGroupAction
 // - OCamlVariablesView#rebuild
 public enum TreeElementGroupKind {
-    EXCEPTIONS("repl.variable.view.exceptions"),
-    MODULE("repl.variable.view.modules"),
-    TYPES("repl.variable.view.types"),
-    FUNCTIONS("repl.variable.view.functions"),
-    VARIABLES("repl.variable.view.variables");
+    EXCEPTIONS(OCamlBundle.message("repl.variable.view.exceptions")),
+    MODULE(OCamlBundle.message("repl.variable.view.modules")),
+    TYPES(OCamlBundle.message("repl.variable.view.types")),
+    FUNCTIONS(OCamlBundle.message("repl.variable.view.functions")),
+    VARIABLES(OCamlBundle.message("repl.variable.view.variables"));
 
     public final String displayName;
 
-    TreeElementGroupKind(@NotNull @PropertyKey(resourceBundle = OCamlBundle.BUNDLE) String key) {
-        this.displayName = OCamlBundle.message(key);
+    TreeElementGroupKind(@NotNull String name) {
+        this.displayName = name;
     }
 }
