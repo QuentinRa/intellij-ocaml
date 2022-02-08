8a837d618b2f4d90b43a645671e264bc846e517b
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2022-02-08 19:40:45 +0100

    not warning "sources missing" if sources are not missing (xD)

--- src/main/com/ocaml/sdk/providers/BaseOCamlSdkProvider.java
+++ src/main/com/ocaml/sdk/providers/BaseOCamlSdkProvider.java
@@ -228,8 +228,8 @@ public class BaseOCamlSdkProvider implements OCamlSdkProvider {
             if (!hasSources) continue;
             // ensure that the directory is not empty
             String[] list = path.toFile().list();
-            sourcesMissing = list == null || list.length > 0;
-            // System.out.println("check "+path+" isn't empty?"+hasSources);
+            sourcesMissing = list == null || list.length == 0;
+//            System.out.println("check "+path+" isn't empty?"+sourcesMissing);
             break;
         }
         if (!hasSources) {
