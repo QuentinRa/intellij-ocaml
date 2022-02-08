cf13f04e846d6860ebf4eb6e7bdaaa6ffd027731
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2022-02-08 20:33:46 +0100

    starting version 0.0.8

---
+++ test/testData/cmt/ex.ml
@@ -0,0 +1,32 @@
+(*
+~/.opam/4.13.0/bin/ocamlc test.ml -bin-annot && rm -rf *.cmo *.cmi a.out
+~/.opam/rincewind/_build/default/bin/rincewind.exe test.cmt
+*)
+
+let x = function ()
+| _ -> ()
+
+(*
+bug
+let () = Format.printf "ok";;
+*)
+
+class type name = object end
+
+(*module Stack = struct
+  class ['a] stack init = object
+  end
+
+  type 'a t = 'a stack
+
+  let make init = new stack init
+end*)
+
+class stack_of_ints =
+    object (self)
+      val mutable the_list = ([] : int list)     (* instance variable *)
+    end;;
+
+let s = new stack_of_ints;;
+
+(* val s : stack_of_ints = <obj> *)
