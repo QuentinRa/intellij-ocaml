90f41b0f0b47bac69d690d881446855b9bdf0317
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2022-02-08 01:32:30 +0100

    #56 - preview auto-selected statement, and pick one

--- src/main/com/ocaml/ide/actions/editor/run/OCamlRunSelection.java
+++ src/main/com/ocaml/ide/actions/editor/run/OCamlRunSelection.java
@@ -3,12 +3,22 @@ package com.ocaml.ide.actions.editor.run;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.util.Pass;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.refactoring.IntroduceTargetChooser;
+import com.intellij.refactoring.introduce.PsiIntroduceTarget;
+import com.ocaml.OCamlBundle;
 import com.ocaml.icons.OCamlIcons;
 import com.ocaml.ide.actions.editor.OCamlEditorActionBase;
 import com.ocaml.ide.console.OCamlConsoleRunner;
 import com.ocaml.utils.adaptor.SinceIdeVersion;
 import com.ocaml.utils.editor.ExtendedEditorActionUtil;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Allow the user to run
@@ -27,8 +37,23 @@ public class OCamlRunSelection extends OCamlEditorActionBase {
     @Override protected void doActionPerformed(@NotNull AnActionEvent e, OCamlConsoleRunner runner) {
         Editor editor = e.getData(CommonDataKeys.EDITOR);
         if (editor == null) return;
-        String code = ExtendedEditorActionUtil.getSelectedCode(editor);
-        if (code == null) return;
-        runner.processCommand(code);
+        PsiFile file = e.getData(CommonDataKeys.PSI_FILE);
+        if (file == null) return;
+        ArrayList<PsiElement> selectedElements = ExtendedEditorActionUtil.getSelectedElements(editor);
+        if (selectedElements == null || selectedElements.size() == 0) return;
+        // only one, do not ask
+        if (selectedElements.size() == 1) {
+            runner.processCommand(selectedElements.get(0).getText());
+            return;
+        }
+        // ask which statement?
+        List<PsiIntroduceTarget<PsiElement>> targets = selectedElements.stream().map(PsiIntroduceTarget::new).collect(Collectors.toList());
+        IntroduceTargetChooser.showIntroduceTargetChooser(editor, targets, new Pass<>() {
+            @Override public void pass(PsiIntroduceTarget<PsiElement> choice) {
+                // just in case
+                if (choice == null || choice.getPlace() == null) return;
+                runner.processCommand(choice.getPlace().getText());
+            }
+       }, OCamlBundle.message("repl.select.a.statement"), 0);
     }
 }
--- src/main/com/ocaml/utils/editor/ExtendedEditorActionUtil.java
+++ src/main/com/ocaml/utils/editor/ExtendedEditorActionUtil.java
@@ -4,6 +4,7 @@ import com.intellij.openapi.editor.Caret;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiElement;
@@ -11,6 +12,8 @@ import com.intellij.psi.PsiFile;
 import com.ocaml.lang.utils.OCamlPsiUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
 
 public class ExtendedEditorActionUtil {
 
@@ -20,12 +23,25 @@ public class ExtendedEditorActionUtil {
      * @param editor the editor
      * @return null if we aren't in a situation in which we can find code to executed,
      * otherwise the selected code
+     * @deprecated this method is returning a string, we need a list of psi tags
      */
+    @Deprecated
     public static @Nullable String getSelectedCode(@NotNull Editor editor) {
         // simply returns the selected code
         // user goal are beyond our understanding
         String code = editor.getSelectionModel().getSelectedText();
         if (code != null && !code.isBlank()) return code;
+        // look for the tag
+        Pair<PsiElement, PsiFile> res = findSelectedElement(editor);
+        if (res == null) return null;
+        // select
+        TextRange range = res.first.getNode().getTextRange();
+        Caret currentCaret = editor.getCaretModel().getCurrentCaret();
+        currentCaret.setSelection(range.getStartOffset(), range.getEndOffset());
+        return res.first.getText();
+    }
+
+    private static @Nullable Pair<PsiElement, PsiFile> findSelectedElement(@NotNull Editor editor) {
         // rip, we need to find what the user want to send to the console
         Project project = editor.getProject();
         if (project == null) return null;
@@ -44,10 +60,23 @@ public class ExtendedEditorActionUtil {
             s = OCamlPsiUtils.findStatementAfter(elementAt);
             if (s == null) return null;
         }
-        // select
-        TextRange range = s.getNode().getTextRange();
-        Caret currentCaret = editor.getCaretModel().getCurrentCaret();
-        currentCaret.setSelection(range.getStartOffset(), range.getEndOffset());
-        return s.getText();
+        return new Pair<>(s, psiFile);
     }
+
+    public static @Nullable ArrayList<PsiElement> getSelectedElements(@NotNull Editor editor) {
+        Pair<PsiElement, PsiFile> statement = findSelectedElement(editor);
+        if (statement == null) return null;
+        PsiElement current = statement.first;
+        ArrayList<PsiElement> candidates = new ArrayList<>();
+
+        // go up until we found the file
+        while (current != statement.second) {
+            // add every candidate
+            candidates.add(0, current);
+            current = current.getParent();
+        }
+
+        return candidates;
+    }
+
 }
