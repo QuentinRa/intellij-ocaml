60597de446dfe42ee88a02f2ee2de7620fff6d5a
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2022-02-13 15:53:54 +0100

    #38 - tests for type inference

--- test/main/com/ocaml/OCamlBaseTest.java
+++ test/main/com/ocaml/OCamlBaseTest.java
@@ -1,6 +1,30 @@
 package com.ocaml;
 
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.vfs.CharsetToolkit;
 import com.intellij.testFramework.fixtures.BasePlatformTestCase;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
+import java.io.IOException;
 
 public class OCamlBaseTest extends BasePlatformTestCase {
+
+    protected @Nullable String loadFile(@NotNull String fileName) {
+        try {
+            return FileUtil.loadFile(new File(getTestDataPath(), fileName), CharsetToolkit.UTF8, true);
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    protected String getCustomTestDataPath() {
+        return "";
+    }
+
+    @Override protected final @NotNull String getTestDataPath() {
+        return "test/testData/"+getCustomTestDataPath();
+    }
+
 }
---
+++ test/testData/com.ocaml.ide.insight.types/types.ml
@@ -0,0 +1,45 @@
+let x = 5
+
+let x = 5
+let y = 5
+
+let x = 5 and y = 5
+
+let x =
+    let y = 5
+    in y
+
+let mx a b = max a b
+
+module type A
+
+module Make1(F: A) = struct end
+
+module Make2 = struct
+    let x = 5
+end
+
+type t = int
+exception E of int * int
+
+let _ = "Hello, world!"
+let rec f x = x
+
+let x = function ()
+| _ -> ()
+
+class type name = object
+	method name : int * int
+end
+
+
+class stack_of_ints =
+    object (self)
+      val mutable the_list = ([] : int list)
+    end
+
+let s = new stack_of_ints;;
+
+module E = Set.Make(
+struct type t = int let compare = compare end
+)
