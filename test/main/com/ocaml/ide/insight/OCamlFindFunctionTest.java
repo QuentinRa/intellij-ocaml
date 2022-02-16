6a18d99ba9563494542c6713428548e5bce95e37
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2022-02-16 16:39:41 +0100

    #38 - first version of the function

--- test/main/com/ocaml/ide/insight/OCamlFindFunctionTest.java
+++ test/main/com/ocaml/ide/insight/OCamlFindFunctionTest.java
@@ -1,9 +1,18 @@
 package com.ocaml.ide.insight;
 
+import com.intellij.openapi.util.Pair;
+import com.intellij.psi.PsiComment;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiWhiteSpace;
+import com.intellij.psi.impl.source.tree.LeafPsiElement;
+import com.intellij.psi.tree.IElementType;
 import com.ocaml.ide.OCamlIdeTest;
+import com.or.lang.OCamlTypes;
 import com.or.lang.core.psi.PsiLet;
+import com.or.lang.core.psi.PsiLowerSymbol;
 import org.intellij.lang.annotations.Language;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.junit.Test;
 
 import java.io.File;
@@ -18,7 +27,7 @@ public class OCamlFindFunctionTest extends OCamlIdeTest {
         return "com.ocaml.ide.insight/";
     }
 
-    private void doTest(@Language("OCaml") String code, String expectedFunctionName, int expectedIndex) {
+    private void assertParameter(@Language("OCaml") String code, String expectedFunctionName, int expectedIndex) {
         @Language("OCaml") String originalMlCode = loadFile(FILE_NAME);
         assertNotNull(originalMlCode);
 
@@ -37,14 +46,34 @@ public class OCamlFindFunctionTest extends OCamlIdeTest {
         File annotations = new File(getTestDataPath(), FILE_NAME_ANNOT);
         annot.updateForFile(elementAt.getContainingFile().getVirtualFile().getPath(), annotations);
 
-        // todo: ...
+        Pair<PsiElement, Integer> data = lookForFunction(caret, 0);
+        assertNotNull(data);
+        assertEquals(expectedFunctionName, data.first.getText());
+        assertEquals(expectedIndex, (int) data.second);
+    }
 
-        assertEquals(expectedFunctionName, "");
-        assertEquals(expectedIndex, -1);
+    private @Nullable Pair<PsiElement, Integer> lookForFunction(@NotNull PsiElement caret, int index) {
+        PsiElement prevSibling = caret.getPrevSibling();
+        System.out.println("with:"+prevSibling.getClass());
+        // skip spaces and comments
+        if (prevSibling instanceof PsiWhiteSpace || prevSibling instanceof PsiComment)
+            return lookForFunction(prevSibling, index);
+        if (prevSibling instanceof LeafPsiElement) {
+            IElementType elementType = ((LeafPsiElement) prevSibling).getElementType();
+            // ref <value> is still the same argument
+            if (elementType.equals(OCamlTypes.REF)) return lookForFunction(prevSibling, index);
+        }
+
+        // name of a function
+        if (prevSibling instanceof PsiLowerSymbol)
+            return new Pair<>(prevSibling, index);
+
+        // no function
+        return null;
     }
 
     @Test
     public void testFunctionGenericWithRef() {
-        doTest("let _ = id ref (*caret*)0", "id", 0);
+        assertParameter("let _ = id ref (*caret*)0", "id", 0);
     }
 }
