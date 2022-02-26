ae64523a9d1dcdc6567ec28baea17fe71e526607
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2022-02-26 15:52:25 +0100

    #75 - fix bug, module SDK can be used instead of the project SDK when creating a REPL

--- src/main/com/ocaml/sdk/utils/OCamlSdkCommandsManager.java
+++ src/main/com/ocaml/sdk/utils/OCamlSdkCommandsManager.java
@@ -12,26 +12,31 @@ import com.intellij.openapi.roots.ProjectRootManager;
 import com.ocaml.OCamlBundle;
 import com.ocaml.sdk.OCamlSdkType;
 import com.ocaml.sdk.providers.OCamlSdkProvidersManager;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
+// note that this class will be removed later, when the REPL will be remade
+@ApiStatus.Internal
 public final class OCamlSdkCommandsManager {
 
+    // find an SDK that can be used to start the REPL
     public static @NotNull Sdk getSdk(Project project) {
         Sdk sdk = ProjectRootManager.getInstance(project).getProjectSdk();
         if (sdk == null || !(sdk.getSdkType() instanceof OCamlSdkType)) {
             for (Module module : ModuleManager.getInstance(project).getModules()) {
                 Sdk sdkCandidate = ModuleRootManager.getInstance(module).getSdk();
-                if (sdk == null || !(sdk.getSdkType() instanceof OCamlSdkType))
+                if (sdkCandidate == null || !(sdkCandidate.getSdkType() instanceof OCamlSdkType))
                     continue;
                 sdk = sdkCandidate;
                 break;
             }
         }
-        if (!(sdk instanceof OCamlSdkType))
+        if (sdk == null || !(sdk.getSdkType() instanceof OCamlSdkType))
             throw new IllegalStateException(OCamlBundle.message("repl.no.sdk"));
         return sdk;
     }
 
+    // returns the command to start the REPL
     public static @NotNull GeneralCommandLine getREPLCommand(@NotNull Project project) {
         Sdk sdk = getSdk(project);
 
