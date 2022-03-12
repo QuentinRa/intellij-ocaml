b23b72cc6a21414a473f64bedb8e2d68cad17c8e
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2022-03-12 22:37:08 +0100

    #90 - disabling the icon is working, insight too

--- src/main/com/ocaml/ide/wizard/minor/settings/OpenOCamlProjectConfigurable.java
+++ src/main/com/ocaml/ide/wizard/minor/settings/OpenOCamlProjectConfigurable.java
@@ -1,23 +1,32 @@
 package com.ocaml.ide.wizard.minor.settings;
 
-import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.options.ex.SingleConfigurableEditor;
 import com.intellij.openapi.options.newEditor.SettingsDialog;
+import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
 import com.ocaml.OCamlBundle;
 import com.ocaml.icons.OCamlIcons;
 import com.ocaml.utils.OCamlPlatformUtils;
 import org.jetbrains.annotations.NotNull;
 
-public class OpenOCamlProjectConfigurable extends AnAction {
+// DumbAwareAction => shown during indexing / ...
+public class OpenOCamlProjectConfigurable extends DumbAwareAction {
+
+    private final boolean shouldEnable;
 
     public OpenOCamlProjectConfigurable() {
         super(OCamlBundle.message("ocaml.project.configuration"), "", OCamlIcons.Nodes.OCAML_SDK);
         // only for non-IntelliJ users
-        getTemplatePresentation().setEnabledAndVisible(
-                !OCamlPlatformUtils.isJavaPluginAvailable()
-        );
+        shouldEnable = !OCamlPlatformUtils.isJavaPluginAvailable();
+    }
+
+    // ...
+    // doing this in the constructor did nothing
+    // ...
+    @Override public void update(@NotNull AnActionEvent e) {
+        super.update(e);
+        e.getPresentation().setEnabledAndVisible(shouldEnable);
     }
 
     public void actionPerformed(@NotNull AnActionEvent e) {
