c2ba753b3e41be1a653e1b773778b500ab1e9176
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2022-03-20 11:22:53 +0100

    detect function in progress

--- src/main/com/ocaml/ide/insight/OCamlParameterInfo.java
+++ src/main/com/ocaml/ide/insight/OCamlParameterInfo.java
@@ -1,13 +1,18 @@
 package com.ocaml.ide.insight;
 
 import com.intellij.lang.parameterInfo.*;
+import com.intellij.openapi.util.Pair;
+import com.intellij.psi.PsiComment;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiWhiteSpace;
 import com.intellij.psi.SyntaxTraverser;
+import com.ocaml.OCamlLanguage;
 import com.ocaml.lang.utils.OCamlPsiUtils;
 import com.ocaml.sdk.annot.OCamlInferredSignature;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -33,40 +38,90 @@ public class OCamlParameterInfo implements ParameterInfoHandler<PsiElement, OCam
     private @Nullable PsiElement findArgumentList(@NotNull ParameterInfoContext context, int parameterListStart) {
         System.out.println("find at("+context.getOffset()+") index ("+parameterListStart+")");
         int offset = context.getOffset() - 1;
-        PsiElement element = context.getFile().findElementAt(offset);
-        if (element == null) return null;
-        System.out.println("  found '"+element.getText()+"' ("+element+")");
+        PsiElement startingElement = context.getFile().findElementAt(offset);
+        if (startingElement == null) return null;
+        System.out.println("  found '"+startingElement.getText()+"' ("+startingElement+")");
 
-        OCamlAnnotResultsService annot = element.getProject().getService(OCamlAnnotResultsService.class);
+        OCamlAnnotResultsService annot = startingElement.getProject().getService(OCamlAnnotResultsService.class);
+
+        // find some start
+        while (startingElement instanceof PsiWhiteSpace || startingElement instanceof PsiComment)
+            startingElement = OCamlPsiUtils.skipMeaninglessPreviousSibling(startingElement);
+        if (startingElement == null) return null;
 
         // find our starting point
-        AtomicReference<OCamlInferredSignature> annotation = new AtomicReference<>();
-        List<PsiElement> psiElements = SyntaxTraverser.psiApi().parents(element).toList();
+        OCamlInferredSignature annotation = null;
+        List<PsiElement> psiElements = SyntaxTraverser.psiApi().parents(startingElement).toList();
         for (PsiElement candidate : psiElements) {
-            annotation.set(annot.findAnnotationFor(candidate, true));
-            element = candidate;
-            if (annotation.get() != null) break;
+            annotation = annot.findAnnotationFor(candidate, true);
+            startingElement = candidate;
+            if (annotation != null) break;
         }
-        if (annotation.get() == null) return null;
+        if (annotation == null) return null;
 
-        System.out.println("  candidate is: '"+element.getText()+"' ("+element+")");
+        System.out.println("  candidate is: '"+startingElement.getText()+"' ("+startingElement+")");
         System.out.println("  annotation found: "+annotation);
 
-        PsiElement psiElement = OCamlPsiUtils.skipMeaninglessPreviousSibling(element);
-        System.out.println("    prev:"+psiElement+" ('"+(psiElement == null ? "null" : psiElement.getText()+"')"));
-        if (psiElement != null) {
-            psiElement = OCamlPsiUtils.skipMeaninglessPreviousSibling(psiElement);
-            System.out.println("    prev:"+psiElement+" ('"+(psiElement == null ? "null" : psiElement.getText()+"')"));
+        ArrayList<Pair<OCamlInferredSignature, PsiElement>> elements = new ArrayList<>();
+        elements.add(new Pair<>(annotation, startingElement));
+
+        PsiElement element = OCamlPsiUtils.skipMeaninglessPreviousSibling(startingElement);
+        int index = 0;
+        int firstFunctionIndex = annotation.type.contains(OCamlLanguage.FUNCTION_SIGNATURE_SEPARATOR) ? 0 : -1;
+        while (element != null) {
+            annotation = annot.findAnnotationFor(element, true);
+            if (annotation == null) break;
+
+            elements.add(0, new Pair<>(annotation, element));
+            element = OCamlPsiUtils.skipMeaninglessPreviousSibling(element);
+            index++;
+            if (firstFunctionIndex != -1)
+                firstFunctionIndex++;
+
+            // we found another function
+            if (annotation.type.contains(OCamlLanguage.FUNCTION_SIGNATURE_SEPARATOR)) {
+                // fix: bypass operators i.e. Stdlib.( + ) for instance
+                String name = annotation.name;
+                System.out.println(name);
+                if (name == null) continue; // not null tho
+                int dot = name.indexOf('.');
+                if (dot != -1) name = name.substring(dot+1);
+                System.out.println(name+" "+dot);
+                // aside from operators
+                if (!name.startsWith("("))
+                    firstFunctionIndex = 0;
+            }
         }
+
+        System.out.println(elements);
+
+        System.out.println("  fun is at index:"+firstFunctionIndex);
+        System.out.println("  we are at index:"+index);
+
+        // first must be a function
+//        if (function.first.kind != OCamlInferredSignature.Kind.VARIABLE) return null;
+//        String type = function.first.type;
+//        if (!type.contains(OCamlLanguage.FUNCTION_SIGNATURE_SEPARATOR)) return null;
+
+        // then we are checking
+//        System.out.println("  function with type:"+type);
+//        System.out.println("  we are at index:"+index);
+
+//        PsiElement psiElement = OCamlPsiUtils.skipMeaninglessPreviousSibling(startingElement);
+//        System.out.println("    prev:"+psiElement+" ('"+(psiElement == null ? "null" : psiElement.getText()+"')"));
+//        if (psiElement != null) {
+//            psiElement = OCamlPsiUtils.skipMeaninglessPreviousSibling(psiElement);
+//            System.out.println("    prev:"+psiElement+" ('"+(psiElement == null ? "null" : psiElement.getText())+"')");
+//        }
 
         return null;
 
-//        OCamlAnnotResultsService annot = element.getProject().getService(OCamlAnnotResultsService.class);
-//        OCamlInferredSignature annotation = annot.findAnnotationFor(element);
+//        OCamlAnnotResultsService annot = startingElement.getProject().getService(OCamlAnnotResultsService.class);
+//        OCamlInferredSignature annotation = annot.findAnnotationFor(startingElement);
 //        if (annotation == null) return null;
 //        // signature:OCamlInferredSignature{name='f', kind=VARIABLE, type=''a -> 'b -> int', range=(23,24)}
 //        System.out.println("signature:"+annotation);
-//        return element;
+//        return startingElement;
     }
 
     @Override
---
+++ test/Notes.md
@@ -0,0 +1,19 @@
+## ParameterInfo
+
+```ocaml
+let _ = (*cursor*)max 5 7
+let _ = max(*cursor*) 5 7
+let _ = max 5(*cursor*) 7
+let _ = max 5 7(*cursor*)
+```
+
+```ocaml
+let _(*cursor*) = max 5 7
+```
+
+```ocaml
+let _ = max (5 + max 5 7) (7 + 3)(*cursor*)
+let _ = max (5 + max 5 7) (7 + 3(*cursor*))
+let _ = max (5 + max 5 7(*cursor*)) (7 + 3)
+let _ = max (5(*cursor*) + max 5 7) (7 + 3)
+```
