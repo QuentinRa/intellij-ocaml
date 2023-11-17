cc036f878120792f1096ba46453993946665b611
Quentin RA <quentin.ramsamy.cf@gmail.com>
2023-11-17 14:04:53 +0100

    Configure build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,46 @@
+import org.jetbrains.kotlin.konan.properties.hasProperty
+import java.util.Properties
+
+// NOTE:
+//
+// The build is controlled by "gradle.properties" variables
+// and the file ".config/gradle-{platformVersion}.properties"
+//
+// You won't have to edit this file unless you want to modify
+// the build "framework" itself.
+
 plugins {
   id("java")
   id("org.jetbrains.kotlin.jvm") version "1.9.0"
   id("org.jetbrains.intellij") version "1.15.0"
 }
 
-group = "com.example"
-version = "1.0-SNAPSHOT"
+// Load Configuration File
+val platformVersion: String by project
+val configFile = file(".config/gradle-${platformVersion}.properties")
+if (!configFile.exists()) {
+  throw GradleException("Configuration file not found: ${configFile.absolutePath}")
+}
+val configProperties = Properties()
+configFile.inputStream().use { input -> configProperties.load(input) }
+
+// Loading Variables
+val majorVersion: String by project
+val minorVersion: String by project
+val patchVersion: String by project
+val platformType: String by project
+val targetVersion: String =
+  if (!configProperties.hasProperty("ideRelease"))
+    configProperties.getProperty("ideVersion")
+  else
+    platformType+"-"+configProperties.getProperty("ideRelease")
+val indicesVersion : String = configProperties.getProperty("indicesVersion")
+val psiViewerPluginVersion : String = configProperties.getProperty("psiViewerPluginVersion")
+val pluginSinceBuild : String = configProperties.getProperty("pluginSinceBuild")
+val pluginUntilBuild : String = configProperties.getProperty("pluginUntilBuild")
+
+group = "com.ocaml"
+version = "$majorVersion.$minorVersion.$patchVersion-$platformVersion"
 
 repositories {
   mavenCentral()
@@ -14,10 +49,13 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-  version.set("2022.2.5")
-  type.set("IC") // Target IDE Platform
+  version.set(targetVersion)
+  // type.set("IC") // Target IDE Platform
 
-  plugins.set(listOf(/* Plugin Dependencies */))
+  plugins.set(listOf(/* Plugin Dependencies */
+    "com.jetbrains.hackathon.indices.viewer:$indicesVersion",
+    "PsiViewer:$psiViewerPluginVersion"
+  ))
 }
 
 tasks {
@@ -31,8 +69,18 @@ tasks {
   }
 
   patchPluginXml {
-    sinceBuild.set("222")
-    untilBuild.set("232.*")
+    sinceBuild.set(pluginSinceBuild)
+    untilBuild.set(pluginUntilBuild)
+    changeNotes.set(
+      """
+      <ul>
+        <li>Can use CTRL-P to display parameters given a function (#38).</li>
+        <li>Can use type inference with ocaml modules (#98).</li>
+        <li>Fixing class not found when using a String</li>
+        <li>See <a href="https://github.com/QuentinRa/intellij-ocaml/blob/main/CHANGELOG.md">CHANGELOG.md</a></li>
+      <ul>
+      """.trimIndent()
+    )
   }
 
   signPlugin {
