1c94f798088f1c5ad8f4b11865ed74e56d152ee8
Quentin RA <quentin.ramsamy.cf@gmail.com>
2023-11-21 11:35:59 +0100

    Adding Color Settings Page

--- src/main/kotlin/com/ocaml/ide/colors/OCamlColor.kt
+++ src/main/kotlin/com/ocaml/ide/colors/OCamlColor.kt
@@ -1,50 +1,53 @@
 package com.ocaml.ide.colors
 
 import com.intellij.openapi.editor.colors.TextAttributesKey
+import com.intellij.openapi.options.OptionsBundle
+import com.intellij.openapi.options.colors.AttributesDescriptor
+import com.ocaml.OCamlBundle
 import com.intellij.openapi.editor.DefaultLanguageHighlighterColors as Default
 
 /**
  * All tokens that can have a color
  */
-enum class OCamlColor(name: String, attr: TextAttributesKey? = null) {
-    VARIABLE("VARIABLE", Default.IDENTIFIER),
-    FIELD("FIELD", Default.INSTANCE_FIELD),
-    CONSTANT("CONSTANT", Default.CONSTANT),
+enum class OCamlColor(humanText: String, attr: TextAttributesKey? = null) {
+    VARIABLE(OCamlBundle.message("settings.ocaml.color.variables.default"), Default.IDENTIFIER),
+    FIELD(OCamlBundle.message("settings.ocaml.color.field"), Default.INSTANCE_FIELD),
+    CONSTANT(OCamlBundle.message("settings.ocaml.color.constant"), Default.CONSTANT),
 
-    FUNCTION("FUNCTION", Default.FUNCTION_DECLARATION),
-    METHOD("METHOD", Default.INSTANCE_METHOD),
-    FUNCTION_CALL("FUNCTION_CALL", Default.FUNCTION_CALL),
-    DIRECTIVE("DIRECTIVE", Default.IDENTIFIER),
+    FUNCTION(OCamlBundle.message("settings.ocaml.color.function.declaration"), Default.FUNCTION_DECLARATION),
+    METHOD(OCamlBundle.message("settings.ocaml.color.method.declaration"), Default.INSTANCE_METHOD),
+    FUNCTION_CALL(OCamlBundle.message("settings.ocaml.color.function.call"), Default.FUNCTION_CALL),
+    DIRECTIVE(OCamlBundle.message("settings.ocaml.color.directives"), Default.IDENTIFIER),
 
-    PARAMETER("PARAMETER", Default.PARAMETER),
-    TYPE_PARAMETER("TYPE_PARAMETER", Default.IDENTIFIER),
-    CONST_PARAMETER("CONST_PARAMETER", Default.CONSTANT),
+    PARAMETER(OCamlBundle.message("settings.ocaml.color.parameter"), Default.PARAMETER),
+    TYPE_PARAMETER(OCamlBundle.message("settings.ocaml.color.type.parameter"), Default.IDENTIFIER),
 
-    TYPE_NAME("TYPE", Default.KEYWORD),
-    CLASS_NAME("CLASS", Default.CLASS_NAME),
-    MODULE_NAME("MODULE", Default.IDENTIFIER),
-    ALIAS("ALIAS", Default.CLASS_NAME),
+    TYPE_NAME(OCamlBundle.message("settings.ocaml.color.type"), Default.KEYWORD),
+    CLASS_NAME(OCamlBundle.message("settings.ocaml.color.class"), Default.CLASS_NAME),
+    MODULE_NAME(OCamlBundle.message("settings.ocaml.color.module"), Default.IDENTIFIER),
+    ALIAS(OCamlBundle.message("settings.ocaml.color.type.alias"), Default.CLASS_NAME),
 
-    KEYWORD("KEYWORD", Default.KEYWORD),
+    KEYWORD(OCamlBundle.message("settings.ocaml.color.keyword"), Default.KEYWORD),
 
-    CHAR("CHAR", Default.STRING),
-    NUMBER("NUMBER", Default.NUMBER),
-    STRING("STRING", Default.STRING),
+    CHAR(OCamlBundle.message("settings.ocaml.color.char"), Default.STRING),
+    NUMBER(OCamlBundle.message("settings.ocaml.color.number"), Default.NUMBER),
+    STRING(OCamlBundle.message("settings.ocaml.color.string"), Default.STRING),
 
-    COMMENT("COMMENT", Default.BLOCK_COMMENT),
+    COMMENT(OCamlBundle.message("settings.ocaml.color.comment"), Default.BLOCK_COMMENT),
 
-    DOC_COMMENT("DOC_COMMENT", Default.DOC_COMMENT),
+    DOC_COMMENT(OCamlBundle.message("settings.ocaml.color.doc.comment"), Default.DOC_COMMENT),
 
-    BRACES("BRACES", Default.BRACES),
-    BRACKETS("BRACKETS", Default.BRACKETS),
-    OPERATORS("OPERATORS", Default.OPERATION_SIGN),
-    SEMICOLON("SEMICOLON", Default.SEMICOLON),
-    DOT("DOT", Default.DOT),
-    COMMA("COMMA", Default.COMMA),
-    PARENTHESES("PARENTHESES", Default.PARENTHESES),
+    BRACES(OptionsBundle.message("options.language.defaults.braces"), Default.BRACES),
+    BRACKETS(OptionsBundle.message("options.language.defaults.brackets"), Default.BRACKETS),
+    OPERATORS(OCamlBundle.message("settings.ocaml.color.operation.sign"), Default.OPERATION_SIGN),
+    SEMICOLON(OptionsBundle.message("options.language.defaults.semicolon"), Default.SEMICOLON),
+    DOT(OptionsBundle.message("options.language.defaults.dot"), Default.DOT),
+    COMMA(OptionsBundle.message("options.language.defaults.comma"), Default.COMMA),
+    PARENTHESES(OptionsBundle.message("options.language.defaults.parentheses"), Default.PARENTHESES),
 
-    ATTRIBUTE("ATTRIBUTE", Default.METADATA),
+    ATTRIBUTE(OCamlBundle.message("settings.ocaml.color.attribute"), Default.METADATA),
     ;
 
     val textAttributesKey = TextAttributesKey.createTextAttributesKey("com.ocaml.$name", attr)
+    val attributesDescriptor = AttributesDescriptor(humanText, textAttributesKey)
 }
---
+++ src/main/kotlin/com/ocaml/ide/settings/OCamlColorSettingsPage.kt
@@ -0,0 +1,81 @@
+package com.ocaml.ide.settings
+
+import com.intellij.openapi.editor.colors.TextAttributesKey
+import com.intellij.openapi.fileTypes.SyntaxHighlighter
+import com.intellij.openapi.options.colors.AttributesDescriptor
+import com.intellij.openapi.options.colors.ColorDescriptor
+import com.intellij.openapi.options.colors.ColorSettingsPage
+import com.ocaml.icons.OCamlIcons
+import com.ocaml.ide.colors.OCamlColor
+import com.ocaml.ide.highlight.OCamlSyntaxHighlighter
+import javax.swing.Icon
+
+class OCamlColorSettingsPage : ColorSettingsPage {
+    override fun getDisplayName(): String = "OCaml"
+    override fun getIcon(): Icon = OCamlIcons.FileTypes.OCAML_SOURCE
+    override fun getAttributeDescriptors() = Constants.ATTRS
+    override fun getColorDescriptors(): Array<ColorDescriptor> = ColorDescriptor.EMPTY_ARRAY
+    override fun getHighlighter(): SyntaxHighlighter = OCamlSyntaxHighlighter()
+    override fun getAdditionalHighlightingTagToDescriptorMap() = Constants.ANNOTATOR_TAGS
+    override fun getDemoText() = Constants.DEMO_TEXT
+
+    internal object Constants {
+        val ATTRS: Array<AttributesDescriptor> = OCamlColor.values().map{ it.attributesDescriptor }.toTypedArray()
+        val ANNOTATOR_TAGS: Map<String, TextAttributesKey> = OCamlColor.values().associateBy({ it.name }, { it.textAttributesKey })
+        val DEMO_TEXT: String by lazy {
+            """(* Variables and Types *)
+let <VARIABLE>x</VARIABLE> = 10
+let <VARIABLE>y</VARIABLE> = 3.14
+let <VARIABLE>name</VARIABLE> = "OCaml"
+
+(* Pattern Matching *)
+let rec factorial n =
+  match n with
+  | 0 -> 1
+  | _ -> n * factorial (n - 1)
+
+(* Functions *)
+let add a b = a + b
+let square x = x * x
+
+let is_even n =
+  if n mod 2 = 0 then
+    true
+  else
+    false
+
+let divide a b =
+  try
+    a / b
+  with
+  | Division_by_zero -> 0
+
+(* Structures *)
+let list = [1; 2; 3; 4; 5]
+let array = [|1; 2; 3; 4; 5|]
+let pair = (10, "hello")
+type person = { name: string; age: int }
+let john = { name = "John"; age = 30 }
+
+(* Modules *)
+module Math = struct
+  let pi = 3.14
+  let square x = x * x
+end
+
+module type Printable = sig
+  val print : unit -> unit
+end
+
+module Printer (M: Printable) = struct
+  let print_twice () =
+    M.print ();
+    M.print ()
+end
+
+module HelloPrinter = Printer(PrintHello)
+"""
+        }
+    }
+
+}
